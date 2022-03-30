c5c59916f944614f62a9397d80007701d1f44a3e
Ludovic <ludovic.mantov@gmail.com>
2022-03-30 19:39:52 +0200

    [ADD] Ajout canva dialogue scène infirmerie

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class InteractionManager : MonoBehaviour
+{
+
+    [SerializeField] Text _dialogueText;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
